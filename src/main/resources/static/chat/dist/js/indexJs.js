270fdab0fbf4b706fe64e3f41d4cb986d65842dd
ljcnnw <1444123370@qq.com>
2020-01-11 22:51:02 +0800

    commit

---
+++ src/main/resources/static/chat/dist/js/indexJs.js
@@ -0,0 +1,64 @@
+jQuery(document).ready(
+    function () {
+        var token = sessionStorage.getItem("token");
+        var user = sessionStorage.getItem("user");
+        var user2 = JSON.parse(user);
+        resetFriendsList();
+        console.info(user);
+        var socket = new SockJS('/endpoint-websocket'); //连接上端点(基站)
+        stompClient = Stomp.over(socket);			//用stom进行包装，规范协议
+        stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/chat/single/' + user2.id, function (result) {
+                console.info(JSON.parse(result.body));
+            });
+        });
+
+        $("#sendMsgBtn").click(function () {
+            stompClient.send(
+                "/app/singleChat/private",
+                {},
+                JSON.stringify({
+                    'content': $("#context").val(),
+                    'out': user2.id,
+                    'in': user2.id
+                }));
+        })
+    }
+)
+
+//初始化好友列表
+function resetFriendsList() {
+    var friendsList = JSON.parse(sessionStorage.getItem("friendsList"));
+    var str = '';
+    friendsList.forEach(function (val, index) {
+        console.info(val);
+        if(index == 0){
+            str += '<li class="active">';
+        }else {
+            str += '<li>';
+        }
+        str += '<div class="conversation ">';
+        str += '';
+        str += '<div class="user-avatar user-avatar-rounded online">';
+        str += '<div class="chatriq-user chatriq-user-11"></div>';
+        str += '</div>';
+        str += '<div class="conversation__details">';
+        str += '<div class="conversation__name">';
+        str += '<div class="conversation__name--title">' + '用户Id:' + val.id + '用户名：' + val.userName + '</div>';
+        str += '<div class="conversation__time">07:15 PM</div>';
+        str += '</div>';
+        str += '<div class="conversation__message">';
+        str += '<div class="conversation__message-preview">';
+        str += 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta consequuntur accusantium tempora. Ad officiis voluptate neque, deleniti porro necessitatibus aut!';
+        str += '</div>';
+        str += '<span><i class="mdi mdi-pin"></i></span>';
+        str += '</div>';
+        str += '</div>';
+        str += '</div>';
+        str += '</li>';
+    });
+
+    $("#friendsList").html(str);
+
+}
